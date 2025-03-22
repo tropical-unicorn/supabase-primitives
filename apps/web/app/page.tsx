2f5db3fb5a11b2baf276365ac215d2afd9361370
Caleb White <calebawhite@gmail.com>
2025-03-22 16:18:14 -0700

    added more secure logic and logut function (#3)

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,17 +1,10 @@
+import Link from 'next/link';
+
 const HomePage = () => {
   return (
-    <div>
-      <article>
-        <header className="">
-          <h1>Testinasdn asdfn asdnfas</h1>
-          <div>
-            <a href="#">print</a>
-            <a href="#">download</a>
-          </div>
-        </header>
-        <p>Testinasdn asdfn asdnfas</p>
-      </article>
-    </div>
+    <main className='flex flex-col items-center justify-center h-screen'>
+      <Link href='/login'>Login</Link>
+    </main>
   );
 };
 
---
+++ apps/web/components/logout/logout.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { createClient } from '@/utils/supabase/client';
+import { useRouter } from 'next/navigation';
+
+const Logout = () => {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    const supabase = await createClient();
+    await supabase.auth.signOut();
+    router.push('/login');
+  };
+
+  return <button onClick={handleLogout}>Logout</button>;
+};
+
+export default Logout;
